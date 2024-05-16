d8320dc0e0c18a6b030d1c2809b505b7b2e78d2c
Tajamal Iqbal <tajamal.logiqon@gmail.com>
2024-05-16 21:59:29 +0500

    My FrameWork

---
+++ GetUDID/GetUDID/GetData.swift
@@ -0,0 +1,14 @@
+//
+//  GetData.swift
+//  GetUDID
+//
+//  Created by Tajamal on 16/05/2024.
+//
+
+import Foundation
+public class Hello_World {
+    public init(){}
+    public func printSms(messageString: String){
+        print(messageString)
+    }
+}
---
+++ GetUDID/GetUDID/GetUDID.h
@@ -0,0 +1,18 @@
+//
+//  GetUDID.h
+//  GetUDID
+//
+//  Created by Tajamal on 16/05/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GetUDID.
+FOUNDATION_EXPORT double GetUDIDVersionNumber;
+
+//! Project version string for GetUDID.
+FOUNDATION_EXPORT const unsigned char GetUDIDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GetUDID/PublicHeader.h>
+
+
